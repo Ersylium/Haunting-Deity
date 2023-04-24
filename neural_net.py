e8a5282063feda13462d52aeefeffcc0bc72636a
ersylium <ersylium@gmail.com>
2023-04-24 17:28:35 +0800

    added chess feature

--- neural_net.py
+++ neural_net.py
@@ -25,4 +25,60 @@ random_actions = threading.Thread(target=random_pings)
 random_actions.start()
 while True:
     string = input("You: ")
-    print("Haunting Deity: " + very_sophisticated_neural_network_yes_yes(string))
+    if "chess" in string.lower():
+        if "help" in string.lower() or "teach" in string.lower():
+            if "please" in string.lower():
+                try:
+                    import stockfish
+                except ImportError:
+                    print("Fuck me, looks like no help for you. (Import Error, unable to import stockfish, retry or contact Ersylium.)")
+                    continue
+
+                while True:
+                    try:
+                        wait = int(input("How long do you want me to ponder until I make my move?"))
+                    except ValueError:
+                        continue
+                    else:
+                        break
+
+                model = stockfish.Stockfish(path="stockfish.exe", depth=18, parameters={"Threads": 2, "Minimum Thinking Time": wait, "Hash": 2048})
+                moves = str(input("If the game has already started, input the sequence of moves until now. Leave blank if there aren't. Separate with comma. (e.g. e2e4, e7e6)")
+                if moves is not "":
+                    moves_list = moves.split(", ")
+                    model.set_position(moves)
+
+                while True:
+                    color = str(input("What is your color? (white/black)"))
+                    if color is not "white" or color is not "black":
+                        print("Your opinion is invalid.")
+                    else:
+                        break
+
+                if len(moves_list)%2 == 0 and color == "white" or len(moves_list)%2 == 1 and color == "black":
+                    pass
+                else:
+                    newmove = [str(input("New Move (type 'end' if match ended.): ")).lower()]
+                    if newmove[0] == "end":
+                        print("GG twat.")
+                        continue
+                    else:
+                        model.make_moves_from_current_position(newmove)
+
+                while True:
+                    print("Next move:")
+                    model.make_moves_from_current_position(model.get_best_move())
+                    model.get_board_visual(True if color == "white" else False)
+                    nextmove = [str(input("Manuca's move (type 'end' if match ended:")).lower()]
+                    if nextmove[0] == "end":
+                        print("If you won, it's because of me. If you lost, you lot with the aid of a computer. Git gud.")
+                        continue
+                    else:
+                        model.make_moves_from_current_position(nextmove)
+
+            else:
+                print("Play your own matches, now fuck off.")
+
+
+
+
